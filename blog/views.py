e9e42bdf00703675d819020fc540d95492a70ecb
Oleg <olberd@bk.ru>
2023-02-21 10:05:56 +0300

    help.

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,20 @@ def serialize_post(post):
         'title': post.title,
         'teaser_text': post.text[:200],
         'author': post.author.username,
+        'comments_amount': Comment.objects.filter(post=post).count(),
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
         'comments_amount': len(Comment.objects.filter(post=post)),
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
@@ -25,11 +39,13 @@ def serialize_tag(tag):
 
 
 def index(request):
-    most_popular_posts = Post.objects.prefetch_related('author').annotate(num_likes=Count('likes')) \
-                             .order_by('-num_likes')[:5]
 
-    fresh_posts = Post.objects.prefetch_related('author').order_by('published_at')
-    most_fresh_posts = list(fresh_posts)[-5:]
+    most_popular_posts = Post.objects.prefetch_related('author') \
+                                     .annotate(num_likes=Count('likes', distinct=True)) \
+                                     .annotate(Count('comments', distinct=True)) \
+                                     .order_by('-num_likes')[:5]
+
+    most_fresh_posts = Post.objects.prefetch_related('author').order_by('-published_at')[:5]
 
     most_popular_tags = Tag.objects.annotate(num_tags=Count('posts')).order_by('-num_tags')[:5]
 
